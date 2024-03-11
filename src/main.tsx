54e47e90a3fc3b80df776dcc2e4dd12e28cc2d13
Paperkeem <ahharlawhddl@gmail.com>
2024-03-11 21:35:48 +0900

    update: add queries option

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,15 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/route.tsx";
 import PrivateRoter from "./provider/PrivateRoter.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+      gcTime: 60 * 1000,
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
