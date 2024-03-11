039d20dd8a13a2c7c70114579e92e35ac1871969
Paperkeem <ahharlawhddl@gmail.com>
2024-03-11 19:06:19 +0900

    feat: issue page

--- src/router/route.tsx
+++ src/router/route.tsx
@@ -2,12 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../screens/Layout";
 import HomePage from "../screens/HomePage";
 import ErrorPage from "../screens/ErrorPage";
+import IssuePage from "../screens/IssuePage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "/home", element: <HomePage /> }],
+    children: [
+      { path: "/home", element: <HomePage /> },
+      {
+        path: "/issue/:issueId",
+        element: <IssuePage />,
+      },
+    ],
   },
   {
     path: "/*",
