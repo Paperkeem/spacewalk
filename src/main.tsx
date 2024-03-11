da44435271e63f18442cb7b66728e6026e6feb56
Paperkeem <ahharlawhddl@gmail.com>
2024-03-11 19:10:26 +0900

    delete: remove import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import GlobalStyles from "./styles/GlobalStyles.ts";
 import { RouterProvider } from "react-router-dom";
@@ -14,7 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <GlobalStyles />
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
